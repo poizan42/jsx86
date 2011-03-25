8728bcd6de47d97ae7daf675ea86f14b94cf96e7
Kasper F. Brandt <poizan@poizan.dk>
2011-03-25 03:04:58 +0100

    License

--- StringStream.js
+++ StringStream.js
@@ -1,3 +1,22 @@
+/*
+A (readonly) Stream wrapping a string
+Copyright (C) 2011 Kasper Fabæch Brandt
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 StringStream = extend(Stream, function (/*string*/ str, /*int*/ position)
 {
 	this._storage = str;
